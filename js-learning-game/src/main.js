7794d36ae36ba5df1523c98a3f8926c86d452f03
leander <leander.hach@gmail.com>
2022-04-25 02:24:52 +1200

    remove fontawesome

--- js-learning-game/src/main.js
+++ js-learning-game/src/main.js
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { store } from './store'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCircleInfo, faPlus, faPause, faPlay, faCog } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faCircleInfo, faPlus, faPause, faPlay, faCog } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faPlus, faCog, faPause, faPlay, faCircleInfo)
+// ibrary.add(faPlus, faCog, faPause, faPlay, faCircleInfo)
 
 const app = createApp(App)
    
 app.use(store)
-app.component('font-awesome-icon', FontAwesomeIcon)
+// app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
