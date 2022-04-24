26135b421d5db57cff3e47eb336a24125c0bc94a
leander <leander.hach@gmail.com>
2022-04-25 02:55:45 +1200

    hf

--- js-learning-game/.eslintrc.js
+++ js-learning-game/.eslintrc.js
@@ -2,5 +2,7 @@ module.exports = {
   parser: "@babel/eslint-parser",
   parserOptions: {
     requireConfigFile: false,
-  }
+  },
+  extends: ["vue", "standard", "plugin:vue/recommended"],
+  plugins: ["import", "vue"]
 };
