51b75b833caab4f9778745d5f2e7f6a0a517512d
leander <leander.hach@gmail.com>
2022-04-25 02:03:57 +1200

    remove fontawesome pro

--- js-learning-game/src/main.js
+++ js-learning-game/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import { store } from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCircleInfo, faPlus, faPause, faPlay, faCog } from '@fortawesome/pro-solid-svg-icons'
+import { faCircleInfo, faPlus, faPause, faPlay, faCog } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faPlus, faCog, faPause, faPlay, faCircleInfo)
